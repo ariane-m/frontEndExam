ad43a431c5e70d27588605d4b1ec11c868745917
ariane <ariane.mejia0316@gmail.com>
2020-03-08 23:52:52 +0800

    mobile view

--- src/scripts/modules/theme-module.js
+++ src/scripts/modules/theme-module.js
@@ -15,6 +15,20 @@ AppName.Modules.ThemeModule = (function() {
         el: '.swiper-pagination',
         clickable: true,
       },
+      breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+      }
     });
   };
 
